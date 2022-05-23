447eded506de45acd70afbd1e68940a272018e77
terellc <terell_case@outlook.com>
2022-05-22 22:04:15 -0400

    cheers

--- classwork/Labs/Lab 5.10/script.js
+++ classwork/Labs/Lab 5.10/script.js
@@ -1,2 +1,5 @@
 // TODO: Write your code here!
-
+$("#myModal .btn-primary").click(function() {
+    $("#yesAlert").show();
+ });
+ 
--- classwork/Labs/Lab 6.15/script.js
+++ classwork/Labs/Lab 6.15/script.js
@@ -1,5 +1,15 @@
 function drawTriangle(size) {
-
-   // Your solution goes here
+   var width = 0;
+   var w;
+   var h;
+   var line; 
    
+   for(h = 0; h < size; h++) {
+        line = "*";
+        for (w = 0; w < width; w++) {
+          line += "*";
+        }
+        console.log(line);
+        width++;
+   }
 }
